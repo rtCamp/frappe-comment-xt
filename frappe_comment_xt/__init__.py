6a36bc71467d9823672af75561bce32753ef58dc
dpk404 <deepak.vkumar@rtcamp.com>
2026-04-28 09:06:32 +0000

    Refactor patch_add_comments_in_timeline to include early return for uninstalled app

--- frappe_comment_xt/__init__.py
+++ frappe_comment_xt/__init__.py
@@ -4,7 +4,11 @@ __version__ = "0.0.1"
 
 
 def patch_add_comments_in_timeline():
+    import frappe
+    if "frappe_comment_xt" not in frappe.get_installed_apps():
+        return
     import frappe.desk.form.load as frappe_load
+
 
     from frappe_comment_xt.helpers.comment import add_comments_in_timeline
 
