497f7b93bc36380c6178e8b34c399019f8a6fa93
Niraj Gautam <nirajgautam196@gmail.com>
2026-05-22 13:32:20 +0530

    chore: Add validation to check if app is in installed apps

--- frappe_comment_xt/monkey_patch.py
+++ frappe_comment_xt/monkey_patch.py
@@ -1,8 +1,13 @@
 def patch_add_comments_in_timeline():
     import frappe.desk.form.load as frappe_load
+    from frappe import get_installed_apps
 
     from frappe_comment_xt.helpers.comment import add_comments_in_timeline
 
+    # in shared bench this is required so it won't break other apps that use add_comments in their codebase.
+    # This is a safe check as the patch will only be applied if the app is installed in the site.
+    if "frappe_comment_xt" not in get_installed_apps():
+        return
     # A monkey patch was written for this function as it is used in many places within Frappe. Care was taken to avoid breaking existing code.
     try:
         frappe_load.add_comments = add_comments_in_timeline
