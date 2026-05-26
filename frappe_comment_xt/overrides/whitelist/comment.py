6a568e27538dca6900bc7600fe8d4920077129b7
trufurs <rajsinghjain.jp@gmail.com>
2026-05-26 10:54:30 +0000

    refactor: Remove unused TYPE_CHECKING import and type hint from add_comment_override

--- frappe_comment_xt/overrides/whitelist/comment.py
+++ frappe_comment_xt/overrides/whitelist/comment.py
@@ -1,5 +1,3 @@
-from typing import TYPE_CHECKING
-
 import frappe
 from frappe.core.doctype.file.utils import extract_images_from_html
 from frappe.desk.doctype.notification_log.notification_log import enqueue_create_notification
@@ -8,9 +6,6 @@ from frappe.utils import strip_html_tags
 from frappe.utils.html_utils import clean_email_html
 
 from frappe_comment_xt.helpers.comment import filter_comments_by_visibility, get_mention_user, get_thread_participants
-
-if TYPE_CHECKING:
-    from frappe.core.doctype.comment.comment import Comment
 
 
 @frappe.whitelist(methods=["POST", "PUT"])
@@ -22,7 +17,7 @@ def add_comment_override(
     comment_by: str,
     custom_visibility: str = "Visible to everyone",
     custom_reply_to: str | None = None,
-) -> Comment:
+):
     """Allow logged user with permission to read document to add a comment"""
     reference_doc = frappe.get_doc(reference_doctype, reference_name)
     reference_doc.check_permission()
